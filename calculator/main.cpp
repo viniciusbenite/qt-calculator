b6608d7b3d71f4b1ad655daa641a2236d462e1fa
Vinícius Benite Ribeiro <vinicius.ribeiro@exaud.com>
2020-11-05 18:46:50 +0000

    Refactor: files rename change

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -1,11 +1,11 @@
-#include "mainwindow.h"
+#include "calculator.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    Calculator w;
     w.show();
     return a.exec();
 }
